82fee7455728be32b3d1e02fac12bd00c5ecfeaf
ALESSANDRO-593 <correo@hotmial.com>
2026-07-13 02:53:38 -0500

    feat: crear estructura basica de la clase Cliente

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ class Producto:
     def mostrar_detalle(self):
         return f"Producto: {self.nombre} | Precio: ${self.precio} | Stock: {self.cantidad}"
 
+
+class Cliente:
+    def __init__(self, nombre, id_cliente, correo):
+        self.nombre = nombre
+        self.id_cliente = id_cliente
+        self.correo = correo
+
+
 # Pruebas unitarias basicas
 if __name__ == "__main__":
     prod = Producto("Laptop", 1200, 5)
